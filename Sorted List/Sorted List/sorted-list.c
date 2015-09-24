3206c635b6c38f4c49d0335ac69f8f533a7ffd41
vinceix <vincentxie12@gmail.com>
2015-09-24 17:23:13 -0400

    Initial Commit

---
+++ Sorted List/Sorted List/sorted-list.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sorted List
+//
+//  Created by Vince Xie, Jacob Rizer on 9/24/15.
+//  Copyright © 2015 Vince Xie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
