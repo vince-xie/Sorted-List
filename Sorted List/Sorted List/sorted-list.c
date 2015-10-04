07b84782d8e5ed4a4b434e1cd70b2f672cbf448a
vinceix <vincentxie12@gmail.com>
2015-10-03 22:38:12 -0400

    Added main

--- Sorted List/Sorted List/sorted-list.c
+++ Sorted List/Sorted List/sorted-list.c
@@ -224,33 +224,3 @@ void * SLNextItem(SortedListIteratorPtr iter){
     }
     return iter->current->data;
 }
-
-/*
- * Prints out the list. For testing purposes only. MUST change printf statement depending on type.
- */
-void printList(SortedListPtr list){
-    SortedListIteratorPtr it = SLCreateIterator(list);
-    it->current = list->head;
-    printf("%s %d\n", it->current->data, it->current->refrences);
-    while(it->current->next != NULL){
-        printf("%s %d\n", SLNextItem(it), it->current->refrences);
-    }
-    SLDestroyIterator(it);
-}
-
-int main(int argc, const char * argv[]) {
-    SortedListPtr list = SLCreate( (int(*)( void *, void * ))strcasecmp , (void(*)(void *))SLDestroy);
-    SLInsert(list, "Hello");
-    SLInsert(list, "ZZZ");
-    SLInsert(list, "Awesome");
-    SLInsert(list, "apple");
-    SLInsert(list, "Zed");
-    SLInsert(list, "Time");
-    SLInsert(list, "Rate");
-    SLInsert(list, "Test");
-    printList(list);
-    printf("%d\n", SLInsert(list, "Time"));
-    printf("%d\n", SLInsert(list, "apple"));
-    printf("%d\n", SLInsert(list, "ZZZ"));
-    return 0;
-}
